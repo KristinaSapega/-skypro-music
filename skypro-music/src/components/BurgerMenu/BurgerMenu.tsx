d39222b8923c8dee45487b35fb8f90b2b55349e2
Kristina <128722669+KristinaSapega@users.noreply.github.com>
2024-10-11 20:29:48 +0300

    the menu opens/closes when clicked BurgerMenu

--- skypro-music/src/components/BurgerMenu/BurgerMenu.tsx
+++ skypro-music/src/components/BurgerMenu/BurgerMenu.tsx
@@ -1,8 +1,13 @@
-import styles from "./BurgerMenu.module.css"
+import React from "react";
+import styles from "./BurgerMenu.module.css";
 
-export const BurgerMenu = () => {
+interface BurgerMenuProps {
+    toggleMenu: () => void;
+}
+
+export const BurgerMenu: React.FC<BurgerMenuProps> = ({toggleMenu}) => {
     return (
-        <div className={styles.navBurger}>
+        <div className={styles.navBurger} onClick={toggleMenu}>
       <span className={styles.burgerLine}></span>
       <span className={styles.burgerLine}></span>
       <span className={styles.burgerLine}></span>
