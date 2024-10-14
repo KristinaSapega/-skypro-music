703200303c4699b020811d6d165af7b2a81e1b75
Kristina <128722669+KristinaSapega@users.noreply.github.com>
2024-10-14 17:22:25 +0300

    changed when clicking from play to pause and vice versa

--- skypro-music/src/components/Bar/Bar.tsx
+++ skypro-music/src/components/Bar/Bar.tsx
@@ -89,9 +89,16 @@ export const Bar = ({currentTrack}: props) => {
                                 </svg>
                             </div>
                             <div onClick={onTogglePLay} className={`${styles.playerBtnPlay} ${styles.btn}`}>
-                                <svg className={styles.playerBtnPlaySvg}>
-                                    <use xlinkHref="/img/icon/sprite.svg#icon-play"></use>
+                            {isPlay ? (
+                                    <svg width="15" height="20" viewBox="0 0 15 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <rect x="0" y="0" width="5" height="20" fill="#D9D9D9" />
+                                    <rect x="10" y="0" width="5" height="20" fill="#D9D9D9" />
                                 </svg>
+                                ) : (
+                                    <svg className={styles.playerBtnPlaySvg}>
+                                        <use xlinkHref="/img/icon/sprite.svg#icon-play"></use>
+                                    </svg>
+                                )}
                             </div>
                             <div className={styles.playerBtnNext}>
                                 <svg className={styles.playerBtnNextSvg}>
