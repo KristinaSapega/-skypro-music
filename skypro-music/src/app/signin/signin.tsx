d1392e2def0a58e2e0ee98a63ebec2a4ff03a88a
Sapega Kristina <tinasapega@gmail.com>
2024-10-31 23:42:06 +0300

    signin file changed

--- skypro-music/src/app/signin/signin.tsx
+++ skypro-music/src/app/signin/signin.tsx
@@ -2,44 +2,106 @@
 
 import Image from "next/image"
 import styles from "./signin.module.css"
+import { useAppDispatch, useAppSelector } from "@/store/store";
+import { useRouter } from "next/router";
+import { useState } from "react";
+import { loginUser, tokenUser } from "@/store/features/authSlice";
+
+export const Signin = () => {
+  const dispatch = useAppDispatch();
+  const router = useRouter();
+  const { errorMessage } = useAppSelector(state => state.auth);
+
+  const [formData, setFormData] = useState({
+    email: "",
+    password: ""
+  });
+
+  const [error, setError] = useState<string | null>(null);
+
+  const handleSignin = async (e: React.FormEvent<HTMLFormElement>) => {
+
+    if (!formData.email) {
+      setError("Введите логин");
+      return;
+    }
+    if (!formData.password) {
+      setError("Введите пароль");
+      return;
+    }
+    if (formData.password.length < 6) {
+      setError("Пароль должен быть не менее 6 символов");
+      return;
+    }
+    try {
+      await dispatch(loginUser({
+        email: formData.email,
+        password: formData.password
+      })
+      ), tokenUser({
+        email: formData.email,
+        password: formData.password})
+    }
+  }
 
 
-<div className={styles.wrapper}>
+
+  const handleMainPage = () => {
+    router.push("/")
+  }
+
+  return (
+    <div className={styles.wrapper}>
       <div className={styles.containerEnter}>
         <div className={styles.modalBlock}>
-          <form 
-          className={styles.modalFormLogin } 
-          action="#">
+          <form
+            className={styles.modalFormLogin}
+            action="#">
             <a>
               <div className={styles.modalLogo}>
                 <Image
-                 src="/img/logo_modal.png" 
-                 alt="logo"
-                 width={140}
-                  height={21} 
-                  />
+                  onClick={handleMainPage}
+                  src="/img/logo_modal.png"
+                  alt="logo"
+                  width={140}
+                  height={21}
+                />
               </div>
             </a>
             <input
+              onChange={(e) =>
+                setFormData({ ...formData, email: e.target.value })
+              }
               className={`${styles.modalInput} ${styles.login}`}
               type="text"
               name="login"
               placeholder="Почта"
             />
             <input
+              onChange={(e) =>
+                setFormData({ ...formData, password: e.target.value })
+              }
               className={`${styles.modalInput} ${styles.password}`}
               type="password"
               name="password"
               placeholder="Пароль"
             />
-            <button className={styles.modalBtnEnter}>
+            <button type="submit" className={styles.modalBtnEnter}>
               <a>Войти</a>
             </button>
             <button className={styles.modalBtnSignup}>
-              <a>Зарегистрироваться</a>
+              <a href="/signup">Зарегистрироваться</a>
             </button>
           </form>
-          <div className={styles.error}></div>
+          {(error || errorMessage) && (
+            <div className={styles.error}>
+              {error || errorMessage}
+            </div>
+          )}
         </div>
       </div>
     </div>
+
+  )
+}
+
